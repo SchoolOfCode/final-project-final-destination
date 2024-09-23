9e314957c7a541a5a18e1ccff54b2b5bb961fdff
Stace0x <167257706+Stace0x@users.noreply.github.com>
2024-09-23 14:48:35 +0100

    test workflow

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <header>Hello World!!</header>
+      <header>Hello World!</header>
       <main>
         <p>boiler plate - test deploy</p>
       </main>

e4d7ce85b8d0948e85e5df80dcc1e282205d2f03
Stace0x <167257706+Stace0x@users.noreply.github.com>
2024-09-23 14:22:18 +0100

    inital commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <header>Hello World!!</header>
+      <main>
+        <p>boiler plate - test deploy</p>
+      </main>
+      <footer>team FD</footer>
+    </div>
+  );
+}

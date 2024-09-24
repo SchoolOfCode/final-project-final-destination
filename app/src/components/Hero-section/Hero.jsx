33bcaedd8b39864cea7d47baaf0a195c236da5ba
Mahik <mahik09@outlook.com>
2024-09-24 15:43:58 +0100

    main section started

--- app/src/components/Hero-section/Hero.jsx
+++ app/src/components/Hero-section/Hero.jsx
@@ -5,8 +5,9 @@ import styles from "./Hero.module.css";
 export default function Hero() {
   return (
     <div className={styles.container}>
-      <div className={styles.hero}></div>
-      <Image src="/images/kids-football.jpg" width="100" height="100"></Image>
+      <div className={styles.hero}>
+        <Image src="/kids-football.jpg" width="100" height="100" />
+      </div>
       <div className={styles.certified}>
         <h2>Certified</h2>
         <p>Our coaches are DBS certified and first aid trained</p>
---
+++ app/src/components/main-section/main.jsx
@@ -0,0 +1,9 @@
+import styles from './main.module.css';
+import React from 'react';
+import Image from 'next/image';
+
+export default function Main() {
+    return (
+        
+    )
+}

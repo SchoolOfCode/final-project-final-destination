f12a7de6d8b14df13535be5a23c8fb3e4685aa79
Faisal Ahmed <git@fais.al>
2024-09-25 15:41:17 +0200

    return only the rows, which is an array of event objects

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -3,7 +3,7 @@ import {pool} from "@/db/script/index"
 
 
 export async function GET(){
-    let  databaseResponse= await pool.query("SELECT * FROM meetups LIMIT 4;")
-    return NextResponse.json(databaseResponse)
+    let  res = await pool.query("SELECT * FROM meetups LIMIT 4;")
+    return NextResponse.json(res.rows)
 }
 

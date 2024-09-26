bf5e0c227ccf7521cba9f713c5da1427ad56fe3f
Mahik <mahik09@outlook.com>
2024-09-26 12:34:29 +0100

    fixed text in hero section

--- app/src/components/Hero/Hero.jsx
+++ app/src/components/Hero/Hero.jsx
@@ -10,13 +10,16 @@ export default function Hero() {
           <h2>Connect and Play Football</h2>
           <p>Join local football meetups for children aged 7-12</p>
           <section className={styles.about}>
-            <h3>Footie Friends: Connecting Young Football Enthusiasts and Their Families</h3>
+            <h3>
+              Footie Friends: Connecting Young Football Enthusiasts and Their
+              Families
+            </h3>
             <p>
-            
-            Join local football meetups for children aged 7-12. 
-            With Footie Friends, finding fun, safe, and age-appropriate football sessions has never been easier. 
-            Browse upcoming events, meet other football-loving families, and create lasting friendships on and off the pitch. 
-            Let’s bring the community together, one kick at a time!
+              With Footie Friends, finding fun, safe, and age-appropriate
+              football sessions has never been easier. Browse upcoming events,
+              meet other football-loving families, and create lasting
+              friendships on and off the pitch. Let’s bring the community
+              together, one kick at a time!
             </p>
           </section>
         </div>
